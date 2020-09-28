949dc95dbe5d792b99a2b98471c7449f064e302a
user74 <housechel@mail.ru>
2020-09-28 18:06:33 +0500

    Lesson 002 02,08-10 - Completed

--- lesson_002/02_operations.py
+++ lesson_002/02_operations.py
@@ -11,3 +11,5 @@ result = (1 + 2) * 3
 print(result)
 
 print(((1+2)*3-4)*5)
+
+# зачет!
--- lesson_002/08_garden.py
+++ lesson_002/08_garden.py
@@ -16,14 +16,16 @@ meadow_set = set(meadow)
 
 # выведите на консоль все виды цветов
 
-print(garden_set|meadow_set)
+print(garden_set | meadow_set)
 
 # выведите на консоль те, которые растут и там и там
 
-print(garden_set&meadow_set)
+print(garden_set & meadow_set)
 
 # выведите на консоль те, которые растут в саду, но не растут на лугу
-print(garden_set-meadow_set)
+print(garden_set - meadow_set)
 
 # выведите на консоль те, которые растут на лугу, но не растут в саду
-print(meadow_set-garden_set)
+print(meadow_set - garden_set)
+
+# зачет!
