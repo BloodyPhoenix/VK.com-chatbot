43d92cb023a9de1488c01f081edc96adf6edec08
user74 <housechel@mail.ru>
2020-11-17 15:56:33 +0500

    Lesson 006 02 - Corrected

--- lesson_006/snowfall.py
+++ lesson_006/snowfall.py
@@ -42,39 +42,33 @@ def move_snowflakes():
 
 def fallen_flakes_numbers():
     fallen_flakes = []
-    # TODO можно было записать вот так:
-    # for index, snowflake in enumerate(_snowflakes):
-    #     if snowflake[1] < 0:
-    #         fallen_flakes.append(index)
+    # TODO почему в большей части нужно делать так, это более питонический подход
+    # TODO Посмотрите статью по этому поводу.
+    # TODO https://habr.com/ru/company/ruvds/blog/485648/
 
-    # Нет. Потому что, если мы проверяем первое от снежинки без её длины, умноденной на два, она удаляется раньше,
-    # чем долетает до конца экрана. Теоретически может быть запись
-    # for index, snowflake in enumerate(_snowflakes):
-    # if snowflake[1] < 0-snowflake[2]*2:
-    #         fallen_flakes.append(index)
-    # Я просто предпочла явное именование переменных координаты y  и длины снежинки, чтобы было понятно, что тут
-    # происходит. В таком варианте я не виду никакого смысла применять enumerate, так как будет то же самое, только
-    # ещё добавится переменная snowflake
-
-    for i in range (len(_snowflakes)):
+    for i in range(len(_snowflakes)):
         snowflake_y = _snowflakes[i][1]
         snowflake_lenght = _snowflakes[i][2]
+        # TODO для удобочитаемости 0-(snowflake_lenght)*2 вынесете в отдельную переменную, скобки не нужны!
         if snowflake_y < 0-(snowflake_lenght)*2:
             fallen_flakes.append(i)
     return fallen_flakes
 
 
-
-# TODO для того чтобы удалять снежинки по индексам вам нужно
-# TODO в цикле получить те самые индексы, но предварительно список fallen_flakes отсортировать и перевернуть
-
 # Зачем его переворачивать, если он УЖЕ содержит индексы? От того, что я его переверну, значения индексов не изменятся.
+# TODO чтобы удаление было с конца, допустим у нас список [5, 9] если мы удалим индекс 5, длинна сместиться
+# TODO и 9 индекса уже не будет!
 # счёт обнуления fallen_flakes - он пересчитывается заново при отрисовке каждого кажра, зачем его обнулять?
+# TODO Хорошо давайте посмотрим как будет работать скрипт с этими изменениями.
+
 
 def delete_snowflakes(snowflakes_numbers):
     global _snowflakes
+    # TODO исправить, писал вам про это в прошлых туду про 0
     for i in range(0, len(_snowflakes)):
         if i in snowflakes_numbers:
+            # TODO отрисовывать тут мы ничего не должны, только удаляем! Может быть по этому сразу было не видно
+            # TODO что происходит с снежинками если список не перевернуть
             snowflake_x = _snowflakes[i][0]
             snowflake_y = _snowflakes[i][1]
             snowflake_length = _snowflakes[i][2]
