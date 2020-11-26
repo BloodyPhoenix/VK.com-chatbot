cb8b30284ce80a1081054138d54d181acc67cb8b
Chashchina Maria <mchashchina@gmail.com>
2020-11-27 02:24:44 +0300

    Tasks 01, 02 completed.

--- lesson_007/01_snowfall.py
+++ lesson_007/01_snowfall.py
@@ -2,6 +2,7 @@
 
 import simple_draw as sd
 
+
 # Шаг 1: Реализовать падение снежинки через класс. Внести в методы:
 #  - создание снежинки с нужными параметрами
 #  - отработку изменений координат
@@ -9,35 +10,78 @@ import simple_draw as sd
 
 
 class Snowflake:
-    pass
 
-    # TODO здесь ваш код
+    fallen_flakes = 0
+    flakes = []
+    count=20
+
+    def get_flakes():
+        for _ in range(Snowflake.count):
+            Snowflake.flakes.append(Snowflake())
+
+    def get_fallen_flakes():
+        for i, flake in enumerate(Snowflake.flakes):
+            if not flake.can_fall():
+                del Snowflake.flakes[i]
+                Snowflake.fallen_flakes += 1
 
 
-flake = Snowflake()
+    def append_flakes():
+        Snowflake.count = Snowflake.fallen_flakes
+        Snowflake.get_flakes()
+        Snowflake.fallen_flakes = 0
 
-while True:
-    flake.clear_previous_picture()
-    flake.move()
-    flake.draw()
-    if not flake.can_fall():
-        break
-    sd.sleep(0.1)
-    if sd.user_want_exit():
-        break
+    def __init__(self):
+        max_x = sd.resolution[0] - 100
+        max_y = sd.resolution[1] + 200
+        min_y = sd.resolution[1] + 100
+        self.y = sd.random_number(min_y, max_y)
+        self.x = sd.random_number(100, max_x)
+        self.lenght = sd.random_number(20, 50)
+        self.center = sd.get_point(self.x, self.y)
 
-# шаг 2: создать снегопад - список объектов Снежинка в отдельном списке, обработку примерно так:
-# flakes = get_flakes(count=N)  # создать список снежинок
+    def clear_previous_picture(self):
+        sd.snowflake(self.center, self.lenght, color=sd.background_color)
+
+    def move(self):
+        self.y -= 15
+        self.x += sd.random_number(-10, 10)
+        self.center = sd.get_point(self.x, self.y)
+
+    def draw(self):
+        sd.snowflake(self.center, self.lenght)
+
+    def can_fall(self):
+        return self.y >= 0 - self.lenght
+
+
+# flake = Snowflake()
+#
 # while True:
-#     for flake in flakes:
-#         flake.clear_previous_picture()
-#         flake.move()
-#         flake.draw()
-#     fallen_flakes = get_fallen_flakes()  # подчитать сколько снежинок уже упало
-#     if fallen_flakes:
-#         append_flakes(count=fallen_flakes)  # добавить еще сверху
+#     flake.clear_previous_picture()
+#     flake.move()
+#     flake.draw()
+#     if not flake.can_fall():
+#         break
 #     sd.sleep(0.1)
 #     if sd.user_want_exit():
 #         break
+
+# шаг 2: создать снегопад - список объектов Снежинка в отдельном списке, обработку примерно так:
+
+Snowflake.get_flakes()
+while True:
+    sd.start_drawing()
+    for flake in Snowflake.flakes:
+        flake.clear_previous_picture()
+        flake.move()
+        flake.draw()
+    Snowflake.get_fallen_flakes()  # подчитать сколько снежинок уже упало
+    if Snowflake.fallen_flakes:
+        Snowflake.append_flakes()  # добавить еще сверху
+    sd.finish_drawing()
+    sd.sleep(0.1)
+    if sd.user_want_exit():
+        break
 
 sd.pause()
--- lesson_007/02_alchemy.py
+++ lesson_007/02_alchemy.py
@@ -19,7 +19,164 @@
 #   print(Water(), '+', Air(), '=', Water() + Air())
 #   print(Fire(), '+', Air(), '=', Fire() + Air())
 
-# TODO здесь ваш код
+class Water:
+    def __str__(self):
+        return "Вода"
+
+    def __add__(self, other):
+        if isinstance(other, Air):
+            return Storm()
+        elif isinstance(other, Fire):
+            return Steam()
+        elif isinstance(other, Ground):
+            return Dirt()
+        else:
+            return None
+
+
+class Air:
+    def __str__(self):
+        return "Воздух"
+
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Storm()
+        elif isinstance(other, Fire):
+            return Lightning()
+        elif isinstance(other, Ground):
+            return Dust()
+        else:
+            return None
+
+
+class Fire:
+    def __str__(self):
+        return "Огонь"
+
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Steam()
+        elif isinstance(other, Air):
+            return Lightning()
+        elif isinstance(other, Ground):
+            return Lava()
+        else:
+            return None
+
+
+class Ground:
+    def __str__(self):
+        return "Земля"
+
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Dirt()
+        elif isinstance(other, Air):
+            return Dust()
+        elif isinstance(other, Fire):
+            return Lava()
+        else:
+            return None
+
+
+class Storm:
+    def __str__(self):
+        return "Шторм"
+
+
+class Steam:
+    def __str__(self):
+        return "Пар"
+
+
+class Dirt:
+    def __str__(self):
+        return "Грязь"
+
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Swamp()
+        elif isinstance(other, Air):
+            return Sand()
+        elif isinstance(other, Fire):
+            return Clay()
+
+
+class Lightning:
+    def __str__(self):
+        return "Молния"
+
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Life()
+        else:
+            return None
+
+
+class Dust:
+    def __str__(self):
+        return "Пыль"
+
+
+class Lava:
+    def __str__(self):
+        return "Лава"
+
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Stone()
+        else:
+            return None
+
+
+class Life:
+    def __str__(self):
+        return "Жизнь"
+
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Plankton()
+        elif isinstance(other, Fire):
+            return Ash()
+        elif isinstance(other, Ground):
+            return Moss()
+
+
+class Stone:
+    def __str__(self):
+        return "Камень"
+
+
+class Ash:
+    def __str__(self):
+        return "Пепел"
+
+
+class Clay:
+    def __str__(self):
+        return "Глина"
+
+
+class Moss:
+    def __str__(self):
+        return "Мох"
+
+
+class Swamp:
+    def __str__(self):
+        return "Болото"
+
+class Sand:
+    def __str__(self):
+        return "Песок"
+
+class Plankton:
+    def __str__(self):
+        return "Планктон"
+
+
+print(Water(), "+", Ground(), "=", Water()+Ground(), "+", Fire(), "=", Water() + Ground() + Fire())
+
 
 # Усложненное задание (делать по желанию)
 # Добавить еще элемент в игру.
