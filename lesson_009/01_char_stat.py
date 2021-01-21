20c59541780a44ddf0c40c04914e4598eaef695e
Chashchina Maria <mchashchina@gmail.com>
2021-01-22 00:04:49 +0300

    Tasks 01, 02 completed

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import os.path
+import zipfile
+
 # Подсчитать статистику по буквам в романе Война и Мир.
 # Входные параметры: файл для сканирования
 # Статистику считать только для букв алфавита (см функцию .isalpha() для строк)
@@ -25,7 +28,67 @@
 #   см https://refactoring.guru/ru/design-patterns/template-method
 #   и https://gitlab.skillbox.ru/vadim_shandrinov/python_base_snippets/snippets/4
 
-# TODO здесь ваш код
+
+class InOutBlock:
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self):
+        return
+
+    @staticmethod
+    # TODO Да, это обязательный метод, потому что без смены рабочей директории под Убунтой ничего не работает
+    def find_file_directory(file_name):
+        path = os.getcwd()
+        dirs = os.walk(path)
+        for directory in dirs:
+            normpath = os.path.normpath(directory[0])
+            os.chdir(normpath)
+            if file_name in directory[2]:
+                break
+            else:
+                for file in directory[2]:
+                    if zipfile.is_zipfile(file):
+                        archive = zipfile.ZipFile(file, "r")
+                        content = archive.namelist()
+                        if file_name in content:
+                            archive.extract(file_name)
+                            break
+
+    @staticmethod
+    def count_letters(file_name):
+        InOutBlock.find_file_directory(file_name)
+        letters_counter = {"total": 0}
+        with open(file_name, "r", encoding="cp1251") as file:
+            for line in file:
+                for char in line:
+                    if char.isalpha():
+                        if char in letters_counter:
+                            letters_counter[char] = letters_counter[char]+1
+                            letters_counter["total"] = letters_counter["total"]+1
+                        else:
+                            letters_counter[char] = 1
+        InOutBlock.print_result(letters_counter)
+
+    @staticmethod
+    def print_result(result):
+        print("""+---------+----------+
+|  буква  | частота  |
++---------+----------+""")
+        for key in result:
+            if len(key) < 2:
+                value = result[key]
+                print("|{:^9}|{:^10}|".format(key, value))
+        print("+---------+----------+")
+        value = result["total"]
+        print("|{:^9}|{:^10}|".format("Итого", value))
+        print("+---------+----------+")
+
+
+test = InOutBlock()
+test.count_letters(file_name="voyna-i-mir.txt")
+
 
 # После зачета первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -23,7 +23,74 @@
 #   см https://refactoring.guru/ru/design-patterns/template-method
 #   и https://gitlab.skillbox.ru/vadim_shandrinov/python_base_snippets/snippets/4
 
-# TODO здесь ваш код
+import os.path
+import zipfile
+
+class EventsCounter:
+
+    def __enter__(self):
+        return
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        return
+
+    @staticmethod
+    def switch_directory(file_name):
+        path = os.getcwd()
+        dirs = os.walk(path)
+        for directory in dirs:
+            normpath = os.path.normpath(directory[0])
+            os.chdir(normpath)
+            if file_name in directory[2]:
+                break
+            else:
+                for file in directory[2]:
+                    if zipfile.is_zipfile(file):
+                        archive = zipfile.ZipFile(file, "r")
+                        content = archive.namelist()
+                        if file_name in content:
+                            archive.extract(file_name)
+                            break
+
+    @staticmethod
+    def prepare_file(file_name):
+        event_log = []
+        with open(file_name, "r", encoding="cp1251") as file:
+            for line in file:
+                prepare_log = line.split()
+                log_date = prepare_log[0].strip("[")
+                log_time = prepare_log[1].strip("]")
+                log_status = prepare_log[2]
+                log = (log_date, log_time, log_status)
+                event_log.append(log)
+        return event_log
+
+    @staticmethod
+    def count_noks(file_name):
+        EventsCounter.switch_directory(file_name=file_name)
+        event_log = EventsCounter.prepare_file(file_name)
+        nok_events = {}
+        for event in event_log:
+            if event[-1] == "NOK":
+                nok_event_date = event[0]
+                nok_event_time = event[1][:5]
+                nok_event = str("["+nok_event_date+" "+nok_event_time+"]")
+                if nok_event in nok_events:
+                    nok_events[nok_event] = nok_events[nok_event]+1
+                else:
+                    nok_events[nok_event] = 1
+        EventsCounter.save_results(nok_events)
+
+    @staticmethod
+    def save_results(result):
+        with open("NOK count.txt", "a+", encoding="cp1251") as file:
+            for key in result:
+                value = str(result[key])
+                file.write(key+" "+value)
+
+
+EventsCounter.count_noks("events.txt")
+
 
 # После зачета первого этапа нужно сделать группировку событий
 #  - по часам
