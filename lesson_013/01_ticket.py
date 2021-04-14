661b34388840f5a4774723cc8e7840ff863534a9
Chashchina Maria <mchashchina@gmail.com>
2021-04-14 20:16:02 +0300

    Part 2 completed. requirments.txt added

--- lesson_013/01_ticket.py
+++ lesson_013/01_ticket.py
@@ -10,27 +10,8 @@
 
 from PIL import Image, ImageDraw, ImageFont, ImageColor
 import os
+import argparse
 
-
-def make_ticket(fio, from_, to, date):
-    ticket = os.path.normpath("images/ticket_template.png")
-    ticket = Image.open(ticket)
-    font = ImageFont.truetype("ofont.ru_Romul.ttf", size=14)
-    draw = ImageDraw.Draw(ticket)
-    draw.text((45, 130), text=fio, font=font, fill=ImageColor.colormap["black"])
-    draw.text((45, 200), text=from_, font=font, fill=ImageColor.colormap["black"])
-    draw.text((45, 265), text=to, font=font, fill=ImageColor.colormap["black"])
-    draw.text((280, 265), text=str(date), font=font, fill=ImageColor.colormap["black"])
-    # TODO пробелы в имени не желательно писать
-    filename = "ticket for "+str(fio)+".png"
-    save_path = os.path.join("images", filename)
-    ticket.save(save_path)
-
-
-make_ticket("Ыыы", "Марс", "Плутон", "22.05.2035")
-
-# TODO Делайте вторую часть
-# TODO укажите полное ФИО
 
 # Усложненное задание (делать по желанию).
 # Написать консольный скрипт c помощью встроенного python-модуля argparse.
@@ -41,5 +22,38 @@ make_ticket("Ыыы", "Марс", "Плутон", "22.05.2035")
 #   --date - обязательный, когда летим.
 #   --save_to - необязательный, путь для сохранения заполненнего билета.
 # и заполнять билет.
+
+parser = argparse.ArgumentParser(description="A ticket maker")
+parser.add_argument("ticket_name", default=None, type=str, help="Name of a ticket owner")
+parser.add_argument("ticket_from", default=None, type=str, help="Departure point")
+parser.add_argument("ticket_to", default=None, type=str, help="Destination")
+parser.add_argument("ticket_date",  default=None, type=str, help="A date of departure")
+parser.add_argument("--save_to", default=None, type=str, help="A directory for a new ticket")
+args = parser.parse_args()
+fio = args.ticket_name
+from_ = args.ticket_from
+to = args.ticket_to
+date = args.ticket_date
+path = args.save_to
+
+def make_ticket(fio, from_, to, date, path=None):
+    ticket = os.path.normpath("images/ticket_template.png")
+    ticket = Image.open(ticket)
+    font = ImageFont.truetype("ofont.ru_Romul.ttf", size=14)
+    draw = ImageDraw.Draw(ticket)
+    draw.text((45, 130), text=fio, font=font, fill=ImageColor.colormap["black"])
+    draw.text((45, 200), text=from_, font=font, fill=ImageColor.colormap["black"])
+    draw.text((45, 265), text=to, font=font, fill=ImageColor.colormap["black"])
+    draw.text((280, 265), text=str(date), font=font, fill=ImageColor.colormap["black"])
+    filename = "ticket for "+str(fio)+".png"
+    print(type(ticket))
+    if path:
+        save_path = os.path.join(path, filename)
+    else:
+        save_path = os.path.join("images", filename)
+    ticket.save(save_path)
+
+if __name__ == "main":
+    make_ticket(fio=fio, from_=from_, to=to, date=date)
 
 # TODO добавить файл requirements в корень этого модуля.
